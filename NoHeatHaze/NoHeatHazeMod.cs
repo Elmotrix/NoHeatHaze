173414fd07362e87573b092dd95360822b911a28
Elmotrix <kim.hoftvedt@gmail.com>
2023-02-03 18:54:48 +0100

    Add project files.

---
+++ NoHeatHaze/NoHeatHazeMod.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+using JetBrains.Annotations;
+
+namespace NoHeatHaze
+{
+    [HarmonyPatch(typeof(HeatHaze), nameof(HeatHaze.Apply))]
+    public class NoHeatHazeMod
+    {
+        [UsedImplicitly]
+        public static bool Prefix()
+        {
+            return false;
+        }
+    }
+}
